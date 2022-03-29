7de0e7e381a47c3ba04e5cc6b604cd64fe89a832
Robert Underwood <rr.underwood94@gmail.com>
2022-03-29 14:34:12 -0400

    scalability and opt

--- run_all.sh
+++ run_all.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #PBS -N roibin_sz
-#PBS -l select=8:ncpus=40:mem=370gb:interconnect=hdr:mpiprocs=30,walltime=24:00:00
+#PBS -l select=5:ncpus=40:mem=370gb:interconnect=hdr:mpiprocs=30,walltime=24:00:00
 #PBS -m abe
 #PBS -M robertu@clemson.edu
 #PBS -j oe
@@ -13,6 +13,7 @@ source /home/robertu/git/spack/share/spack/setup-env.sh
 spack env activate .
 module load openmpi/4.0.5-gcc/8.4.1-ucx
 
+echo overview==
 # for cxi_file in "/scratch1/robertu/chuck/cxic00318_0123_0.cxi" "/scratch1/robertu/chuck/cxic0415_0101.cxi"
 # do 
 # for chunk_size in 1 8
@@ -30,18 +31,43 @@ module load openmpi/4.0.5-gcc/8.4.1-ucx
 
 echo smallscale===
 # small-scale quality assessment
-chunk_size=1
-cxi_file=/scratch1/robertu/chuck/cxic00318_0123_0.cxi
-for config in share/roibin_sz3.json share/roibin_sz.json #share/roibin_zfp.json share/roibin_mgard.json share/sz-*.json share/roibin_blosc-9.json share/sz.json share/sz3-*.json share/mgard-*.json share/zfp-*.json
-do
-	mpiexec ./build/roibin_test -c $chunk_size -f "$cxi_file" -o "$cxi_file.$(basename $config)" -p "$config"
-done
-
-# echo fullscale===
-# # full-scale quality assessment
-# config=./share/roibin_sz.json
 # chunk_size=1
-# for cxi_file in /scratch1/robertu/chuck/full_eval/cxic00318_0123_*.cxi
+# cxi_file=/scratch1/robertu/chuck/cxic00318_0123_0.cxi
+# for config in share/roibin_sz3.json share/roibin_sz.json #share/roibin_zfp.json share/roibin_mgard.json share/sz-*.json share/roibin_blosc-9.json share/sz.json share/sz3-*.json share/mgard-*.json share/zfp-*.json
 # do
+#   echo "chunk_size=$chunk_size replica=$replica config=$config filename=$cxi_file"
 # 	mpiexec ./build/roibin_test -c $chunk_size -f "$cxi_file" -o "$cxi_file.$(basename $config)" -p "$config"
 # done
+
+echo fullscale===
+# full-scale quality assessment
+# config=./share/roibin_sz.json
+# chunk_size=1
+# replica=1
+# for cxi_file in /scratch1/robertu/chuck/full_eval/cxic00318_0123_*.cxi
+# do
+#   echo "chunk_size=$chunk_size replica=$replica config=$config filename=$cxi_file"
+# 	mpiexec ./build/roibin_test -c $chunk_size -f "$cxi_file" -o "$cxi_file.$(basename $config)" -p "$config"
+# done
+
+echo scalability===
+replica=1
+config=./share/roibin_sz.json
+chunk_size=1
+cxi_file=/scratch1/robertu/chuck/cxic00318_0123_0.cxi
+for procs in `seq 30 30 150`
+do
+  echo "chunk_size=$chunk_size replica=$replica config=$config filename=$cxi_file"
+	mpiexec -np $procs ./build/roibin_test -c $chunk_size -f "$cxi_file" -p "$config"
+done
+echo 
+
+echo opt===
+replica=1
+chunk_size=1
+cxi_file=/scratch1/robertu/chuck/cxic00318_0123_0.cxi
+for config in share/opt/*.json
+do
+  echo "chunk_size=$chunk_size replica=$replica config=$config filename=$cxi_file"
+	mpiexec ./build/roibin_test -c $chunk_size -f "$cxi_file" -o "$cxi_file.$(basename $config)" -p "$config"
+done
